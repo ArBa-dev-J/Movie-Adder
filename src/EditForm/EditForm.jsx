0ab5e3f4cde03b108463fe28ce80954c722dc4bb
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-01-20 12:58:54 +0200

    add edit form

--- src/EditForm/EditForm.jsx
+++ src/EditForm/EditForm.jsx
@@ -1,9 +1,82 @@
 function EditForm() {
   return (
     <>
-      <section className="bg-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-40 w-2xl m-auto ">
-      <form>
-        
+    <section className="border p-10">
+      <form
+        // onSubmit={handleSubmit(onSumbit)}
+        className="flex flex-row justify-center h-[30px]  items-baseline-last gap-10"
+      >
+        <div>
+          <label htmlFor="title" className="block text-center">
+            Movie Title
+          </label>
+          <input type="text" name="title" className="border mr-2.5" />
+        </div>
+        {/*MOVIE GENRE*/}
+        <div>
+          <div className="border flex gap-2.5">
+            <div>
+              <input type="checkbox" name="genre" value="Drama" />
+              <label htmlFor="rating">Drama</label>
+            </div>
+            <div>
+              <input type="checkbox" name="genre" value="Comedy" />
+              <label htmlFor="rating">Comedy</label>
+            </div>
+            <div>
+              <input type="checkbox" name="genre" value="Action" />
+              <label htmlFor="rating">Action</label>
+            </div>
+            <div>
+              <input type="checkbox" name="genre" value="Horror" />
+              <label htmlFor="rating">Horror</label>
+            </div>
+            <div>
+              <input type="checkbox" name="genre" value="Sci-fi" />
+              <label htmlFor="rating">Sci-fi</label>
+            </div>
+            <div>
+              <input type="checkbox" name="genre" value="Fantasy" />
+              <label htmlFor="rating">Fantasy</label>
+            </div>
+          </div>
+        </div>
+        {/*MOVIE REALESE DATE*/}
+        <div>
+          <label htmlFor="date" className="block">
+            Movie release year
+          </label>
+          <input type="date" name="date" className="border mr-2.5" />
+        </div>
+        {/*MOVIE RATING*/}
+        <div>
+          <div className="border flex gap-2.5">
+            <div>
+              <input type="radio" name="rating" value="1" />
+              <label htmlFor="rating">1</label>
+            </div>
+            <div>
+              <input type="radio" name="rating" value="2" />
+              <label htmlFor="rating">2</label>
+            </div>
+            <div>
+              <input type="radio" name="rating" value="3" />
+              <label htmlFor="rating">3</label>
+            </div>
+            <div>
+              <input type="radio" name="rating" value="4" />
+              <label htmlFor="rating">4</label>
+            </div>
+            <div>
+              <input type="radio" name="rating" value="5" />
+              <label htmlFor="rating">5</label>
+            </div>
+          </div>
+        </div>
+        {/* SUBMIT BUTTON */}
+        <div>
+          <input type="submit" className="border" />
+        </div>
       </form>
       </section>
     </>
